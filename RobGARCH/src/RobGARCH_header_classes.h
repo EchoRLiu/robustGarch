9bb5dd1901d758b398e768e1d21793891660818e
EchoRLiu <59842097+EchoRLiu@users.noreply.github.com>
2020-07-20 23:11:50 -0700

    Add files via upload

--- RobGARCH/src/RobGARCH_header_classes.h
+++ RobGARCH/src/RobGARCH_header_classes.h
@@ -1,4 +1,73 @@
-//#ifndef ROBGARCH_HEADER_CLASSES_H
-//#define ROBGARCH_HEADER_CLASSES_H
+#pragma
+#ifndef ROBGARCH_HEADER_CLASSES_H
+#define ROBGARCH_HEADER_CLASSES_H
 
-#include <cmath>
+#include <iostream>
+#include <vector>
+#include <array>
+
+class RobGarch11
+{
+public:
+  // Constructor: initialize x_, obtain vX_, normX_, vNX_.
+  RobGarch11(const std::vector<double>& x);
+  
+  // Parameters estimations of BM1.
+  void optBM1();
+  // Parameters estimations of BM2.
+  void optBM2();
+  
+  // Accessors: parameters estimations of BM1 and BM2.
+  std::array<double, 3> BM1() const;
+  std::array<double, 3> BM2() const;
+  
+private:
+  
+  void normalize_();
+  double tausq_(const std::vector<double>& x);
+  void sEst_(const std::vector<double>& x);
+  std::vector<double> rho_(const std::vector<double>& x);
+  // More to come.
+  
+  
+  std::vector<double> x_, normX_;
+  double vX_, vNX_, s_;
+  double tausqConst_ = 0.4797;
+  // More to come.
+};
+
+
+
+class Division
+{
+public:
+
+  Division(const std::vector<double> vecN, const double d);
+  Division(const std::vector<double> vecN);
+  // Vector x is strongly preferred -> required?.
+  // Division();
+  
+  // Access the division result.
+  std::vector<double> vecDiv() const;
+  double mean() const; // mean of vecNumer_.
+  std::vector<double> vecNumer() const;
+  double denom() const;
+  void setVecNumer(const std::vector<double> vecNumer);
+  void setDenom(const double denom);
+  
+
+private:
+  
+  std::vector<double> vecNumer_, vecDiv_;
+  double denom_;
+  double mean_;
+  
+  void checkForZero_(double d) const;
+  void getVecDiv_();
+  void getMean_(); // mean of vecNumer_.
+  
+};
+
+
+#endif // ROBGARCH_HEADER_CLASSES_H
+
--- RobGARCH/src/RobGARCH_imple_classes.cpp
+++ RobGARCH/src/RobGARCH_imple_classes.cpp
@@ -1,2 +1,142 @@
 #include "RobGARCH_header_classes.h"
+#include <cstdlib> // std::exit
+//#include <numeric> 
+#include <cmath> // std::sqrt(double), std::pow
+
+using std::cout;
+using std::endl;
+using std::exit;
+using std::vector;
+using std::array;
+using std::sqrt;
+using std::pow;
+using std::accumulate;
+
+RobGarch11::RobGarch11(const vector<double>& x): x_(x)
+{
+  
+  // get v_, vini_; normalize the data to be stored in y_.
+  vX_ = tausq_(x);
+  normalize_(); // Get normX_.
+  vNX_ = tausq_(normX_);
+
+}
+
+void RobGarch11::sEst_(const vector<double>& x)
+{
+  
+  // More to come.
+}
+
+vector<double> RobGarch11::rho_(const vector<double>& x)
+{
+  
+  // More to come.
+}
+
+double RobGarch11::tausq_(const vector<double>& x)
+{
+  sEst_(x);
+  // In R code, s is set as global variable as "Sestim".
+  // Here s_ is private member variable could be accessed.
+  Division xS(x, s_);
+  Division r(rho_(xS.vecDiv()));
+  double t = r.mean() * std::pow(s_, 2) / tausqConst_;
+
+}
+
+void RobGarch11::normalize_(){
+  
+  double sqrtVX = sqrt(vX_);
+  double meanNX = accumulate(x_.begin(), x_.end(), 0.0)/(x_.size()*sqrtVX);
+  
+  for(int i = 0; i < x_.size(); ++i){
+    normX_[i] = x_[i]/sqrtVX - meanNX; // Since there is an extra operation,
+                                       // Division is not used.
+    if(normX_[i] == 0){
+      normX_[i] = 10.0e-10;
+    }
+  }
+}
+
+/////////////////////////////////////////////////////////
+////////////////// Division Class ///////////////////////
+
+Division::Division(const vector<double> vecN): vecNumer_(vecN), denom_(1.0)
+{
+  getMean_();
+}
+
+Division::Division(const vector<double> vecN, const double d): vecNumer_(vecN), denom_(d)
+{
+  checkForZero_(d);
+  getVecDiv_();
+  getMean_();
+}
+
+vector<double> Division::vecDiv() const
+{
+  return vecDiv_;
+}
+
+double Division::mean() const
+{
+  return mean_;
+}
+
+vector<double> Division::vecNumer() const
+{
+  return vecNumer_;
+}
+
+double Division::denom() const
+{
+  return denom_;
+}
+
+void Division::setVecNumer(const vector<double> vecNumer)
+{
+  vecNumer_ = vecNumer;
+  getVecDiv_();
+}
+
+void Division::setDenom(const double denom)
+{
+  checkForZero_(denom);
+  denom_ = denom;
+  getVecDiv_();
+}
+
+void Division::checkForZero_(double denom) const
+{
+  if(denom == 0.0)
+  {
+    std::exit(EXIT_FAILURE);
+  }
+}
+
+void Division::getVecDiv_()
+{
+  for(int i = 0; i < vecNumer_.size(); ++i)
+  {
+    vecDiv_[i] = vecNumer_[i] / denom_;
+  }
+}
+
+void Division::getMean_()
+{
+  double n = vecNumer_.size() + 0.0;
+  if(n == 0.0)
+  { 
+    std::exit(EXIT_FAILURE);
+  }
+  
+  mean_ = 0.0;
+  for(int i = 0; i < vecNumer_.size(); ++i)
+  {
+    mean_ += vecNumer_[i];
+  }
+  mean_ /= n;
+}
+
 
