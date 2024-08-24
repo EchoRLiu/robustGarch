1fcbb2dd4716f452816f3903c9b07e8e1f8d3c52
dan9401 <dansummer94@gmail.com>
2024-08-24 16:40:48 +0000

    resolve s3 methods consistency issue

--- R/robustGARCH-summary.R
+++ R/robustGARCH-summary.R
@@ -2,7 +2,9 @@
 #'
 #' @description Summary for robustGARCH S3 class
 #'
-#' @param fit A robustGARCH fit object of class \code{\link{robustGARCH}}
+#' @param fit A robustGARCH fit object of class \code{\link{robGarch}}
+#' @param x Same as fit, for plot.robustGARCH and print.robustGARCH
+#' @param object Same as fit, for summary.robustGARCH
 #' @param digits the number of digits for print and plot, default is 3.
 #' @param main_name the title of the plot, default is "Conditional SD (vs returns)"
 #' @param estimation_pos string that determines the legend position that specifies gamma, alpha, beta estimations. Choice of "bottomright", "bottom", "bottomleft", "left", "topleft", "top", "topright", "right" and "center". Default is "topleft".
@@ -11,6 +13,8 @@
 #' @param original_ a logical argument. If TRUE, the original return will be plotted. Default is FALSE
 #' @param pctReturn_ a logical argument. IF TRUE, the plot function will plot the returns in percentage instead of original. Default is TRUE.
 #' @param abs_ a logical argument, when TRUE, the plot function will plot abs(returns) with conditional standard deviation instead of returns, default to TRUE.
+#' @param ... # to be written
+#' @param nu # to be written
 #'
 #' @name robustGARCH-summary
 #' @aliases summary.robustGARCH
@@ -21,15 +25,25 @@
 #' @examples
 #'
 #' data("gspc")
-#' fit <- robGarch(gspc, methods="BM", fixed_pars = c(0.8, 3.0), optimizer="Rsolnp", stdErr_method = "numDeriv")
+#' fit <- robGarch(gspc, methods="BM", fixed_pars = c(0.8, 3.0),
+#'                 optimizer="Rsolnp", stdErr_method = "numDeriv")
 #' summary(fit)
 #' print(fit)
 #' plot(fit)
 #' aef(fit)
-#'
+#' 
 #' @export
-summary.robustGARCH <- function(fit, digits = 3){
+summary.robustGARCH <- function(object, digits = 3, ...){
+# summary.robustGARCH <- function(fit, digits = 3){
 
+# notes on why the name change
+# Undocumented arguments in Rd file 'robustGARCH-summary.Rd'
+#      ‘object’
+# Functions with \usage entries need to have the appropriate \alias
+#    entries, and all their arguments documented.
+#    The \usage entries must correspond to syntactically valid R code.
+#    See chapter ‘Writing R documentation files’ in the ‘Writing R
+#    Extensions’ manual.
   res <- rbind(round(fit$fitted_pars, digits), round(fit$standard_error, digits), round(fit$t_value, digits), round(fit$p_value, digits))
   colnames(res) <- names(fit$fitted_pars)
   rownames(res) <- c("Estimates", "Std. Errors", "t-statistic", "p-value")
@@ -55,9 +69,10 @@ summary.robustGARCH <- function(fit, digits = 3){
   cat("\nTime elapsed: ", fit$time_elapsed)
   cat("\nConvergence Message: ", fit$message)
 }
+
 #' @rdname robustGARCH-summary
 #' @export
-print.robustGARCH <- function(fit, digits = 3){
+print.robustGARCH <- function(x, digits = 3, ...){
 
   res <- rbind(round(fit$fitted_pars, digits))
   colnames(res) <- names(fit$fitted_pars)
@@ -75,13 +90,15 @@ print.robustGARCH <- function(fit, digits = 3){
   cat("Result:\n")
   noquote(res)
 }
+
 #' @rdname robustGARCH-summary
 #' @export
-plot.robustGARCH <- function(fit, digits = 3, estimation_pos = "topleft", line_name_pos = "topright", par_ = par(no.readonly = TRUE), pctReturn_ = TRUE, abs_ = TRUE, original_ = FALSE, main_name = "Conditional Volatility (vs |pctReturns(%)|)"){
+plot.robustGARCH <- function(x, digits = 3, estimation_pos = "topleft", line_name_pos = "topright", par_ = par(no.readonly = TRUE), pctReturn_ = TRUE, abs_ = TRUE, original_ = FALSE, main_name = "Conditional Volatility (vs |pctReturns(%)|)", ...){
 
   .plot.garchsim(fit, digits, estimation_pos, line_name_pos, par_, pctReturn_, abs_, original_, main_name)
 
 }
+
 #' @rdname robustGARCH-summary
 #' @export
 aef <- function(fit, nu=5){
@@ -95,5 +112,3 @@ aef <- function(fit, nu=5){
 
   aef
 }
-
-
