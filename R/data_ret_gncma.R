278b0294e20b863f4b8e1213792d294ff6e140c5
dan9401 <dansummer94@gmail.com>
2024-12-09 12:58:19 +0000

    add retGNCMA data

---
+++ R/data_ret_gncma.R
@@ -0,0 +1,15 @@
+#' Daily Return of General Communication from 2006-01-06 to 2010-12-31.
+#' 
+#' @name retGNCMA
+#' @docType data
+#' @usage data(retGNCMA)
+#'
+#' @format A xts object with length 261 weekly data for 1 variable.
+#' \describe{
+#'   \item{log return}{log return}
+#'   ...
+#' }
+#'
+#' @keywords datasets
+#' @importFrom xts xts
+"retGNCMA"
